c7da83176ed423a6517180d2ef920b41ed751e7d
michaeldebetaz <netarion@outlook.com>
2025-06-12 15:14:30 +0200

    fix: rename scrapper to scraper (typo)

--- cmd/unilscrap/main.go
+++ cmd/unilscrap/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"github.com/michaeldebetaz/unilscrap/internal/env"
 	"github.com/michaeldebetaz/unilscrap/internal/logger"
-	"github.com/michaeldebetaz/unilscrap/internal/scrapper"
+	"github.com/michaeldebetaz/unilscrap/internal/scraper"
 )
 
 func main() {
 	logger.Init()
 	env.Load()
 
-	scrapper.Scrape()
+	scraper.Scrape()
 }
